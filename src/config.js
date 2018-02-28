696ad3adcde96256aa00953b0794159774f78324
rosstapson <ross.tapson@gmail.com>
2018-02-28 05:06:05 +0200

    engaged https

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //export const API_ROOT = "https://wt-74fc5e9290c63d70bb357b5d5b1d8d15-0.run.webtask.io/ssetaServer";
-export const API_ROOT = "http://197.97.230.46:8080"
+export const API_ROOT = "https://rhtech.co.za:8080"
 export const ROLES = ['learner', 'teacher', 'manager','trainer', 'admin']
