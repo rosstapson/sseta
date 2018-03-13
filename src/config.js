ca4249b56b93f11fec113298f5d60fb582d6f809
rosstapson <ross.tapson@gmail.com>
2018-03-13 14:50:54 +0200

    removed from tracking

--- src/config.js
+++
@@ -1,3 +0,0 @@
-//export const API_ROOT = "https://wt-74fc5e9290c63d70bb357b5d5b1d8d15-0.run.webtask.io/ssetaServer";
-export const API_ROOT = "https://rhtech.co.za:8080"
-export const ROLES = ['learner', 'teacher', 'manager','trainer', 'admin']
